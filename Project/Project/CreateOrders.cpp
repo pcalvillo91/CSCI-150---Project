32d4715fda9d49957b0241e3c0b0a4297007cc92
Prestin Calvillo <pcalvillo@mail.fresnostate.edu>
2016-11-11 21:52:03 -0800

    update1
    Still work in progress, but heres my start of it

--- Project/Project/CreateOrders.cpp
+++ Project/Project/CreateOrders.cpp
@@ -1,22 +1,71 @@
-//
-//  CreateOrders.cpp
-//  Project
-//
-//  Created by Sanjay Soundarajan on 11/8/16.
-//  Copyright Â© 2016 INSERT TEAM NAME HERE. All rights reserved.
-//
+/*
+CreateOrders.cpp
+Project
+
+Created by Emilio Calvillo on 11/11/16.
+Copyright © 2016 INSERT TEAM NAME HERE. All rights reserved.
+*/
 
 #include "CreateOrders.hpp"
 
 void Orders()
 {
-    
-    
-    
-    // After ordering and dining
+    string m;
+    int i = 1;
+    int exit = 0;
+    int e;
+    int choice;
+
+    while (exit == 0){
+//        cout << "Press exit anytime to return to main menu" << endl;
+
+///Load Current Table Numbers
+
+///Select Table
+        cout << "Input table number for current order" << endl;
+        cin >> table; //Varies depending on how tables are stored
+///Load customers??? Unless already linked to tables
+
+        while (i == 1) {
+///Select Customer
+            cout << "Please input customer number" << endl;
+            cin >> customer; //Varies depending on how customers are stored
+///Load food and drink menu
+
+///Select food or drink
+            cout << "Press 1 for Food, 2 for Drink" << endl;
+            cin >> choice;
+
+            switch (choice) {
+                case 1 :
+//        food
+                    cout << "Food" << endl;
+///Expand food menu, use switch to add customizing on order, push end result to customer's vector
+                    break;
+                case 2 :
+//        drink
+                    cout << "Drink" << endl;
+///Expand drink menu, use switch to add customizing on order, push end result to customer's vector
+                    break;
+                default :
+                    break;
+            }
+            cout << "Are there more customer orders for current table? (Y/N)" << endl;
+            cin >> m;
+            if (m == 'N'){
+                exit = 1;
+                i = 0;
+            }
+        }
+
+
+    }
+
+/// After ordering and dining
     updateStats();
     splitBills();
 }
 
+int customize (){
 
-
+}
--- Project/Project/CreateOrders.hpp
+++ Project/Project/CreateOrders.hpp
@@ -1,10 +1,10 @@
-//
-//  CreateOrders.hpp
-//  Project
-//
-//  Created by Sanjay Soundarajan on 11/8/16.
-//  Copyright Â© 2016 INSERT TEAM NAME HERE. All rights reserved.
-//
+/*
+CreateOrders.hpp
+Project
+
+Created by Emilio Calvillo on 11/11/16.
+Copyright © 2016 INSERT TEAM NAME HERE. All rights reserved.
+*/
 
 #ifndef CreateOrders_hpp
 #define CreateOrders_hpp
@@ -12,6 +12,13 @@
 #include <stdio.h>
 #include "Stats.hpp"
 #include "BillSplit.hpp"
+#include "Table.hpp"
+//#include "Customer.hpp"
+#include "Menu.hpp"
+#include <string>
+#include <iostream>
+//#include <unistd.h>     //for mac
+#include <windows.h>  //for windows
 
 void Orders();
 
