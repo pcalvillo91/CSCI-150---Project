ab72833c1a402a1862b7b7720537175a9595d66f
Prestin Calvillo <pcalvillo@mail.fresnostate.edu>
2016-11-12 19:03:47 -0800

    update2
    next update, still working

--- Project/Project/CreateOrders.cpp
+++ Project/Project/CreateOrders.cpp
@@ -11,10 +11,11 @@ Copyright © 2016 INSERT TEAM NAME HERE. All rights reserved.
 void Orders()
 {
     string m;
+    string a;
     int i = 1;
     int exit = 0;
     int e;
-    int choice;
+    int choice, choicea, choiceb;
 
     while (exit == 0){
 //        cout << "Press exit anytime to return to main menu" << endl;
@@ -30,22 +31,173 @@ void Orders()
 ///Select Customer
             cout << "Please input customer number" << endl;
             cin >> customer; //Varies depending on how customers are stored
-///Load food and drink menu
-
 ///Select food or drink
             cout << "Press 1 for Food, 2 for Drink" << endl;
             cin >> choice;
-
             switch (choice) {
-                case 1 :
-//        food
-                    cout << "Food" << endl;
+                case 1 : //Food
+                    cout << "(1) Lunch (2) Dinner" << endl;
+                    cin >> time;
+                    switch (time) {
+                        case 1 : //Lunch
+                        cout << "What food? (1) Burger (2) Cheese burger (3) fries (4) hotdog (5) bacon cheeseburger"
+                        cin >> choiceb;
+                        switch (choiceb) {
+                            case 1 : //Burger
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Lemon (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //add lemon
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                            case 2 : //Cheese Burger
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Extra Large (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //Make large
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                            case 3 : //Fries
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Lemon (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //add lemon
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                            case 4 : //Hotdog
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Extra Large (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //Make large
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                            case 5 : //Bacon CheeseBurger
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Extra Large (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //Make large
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                        case 2 : //Dinner
+                       cout << "What food? (1) Turkey (2) Chicken (3) Tri-Tip"
+                        cin >> choiceb;
+                        switch (choiceb) {
+                            case 1 : //Turkey
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Lemon (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //add lemon
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                            case 2 : //Chicken
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Extra Large (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //Make large
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                            case 3 : //Tri-Tip
+                                cout << "Customize order? (Y/N)" << endl;
+                                cin >> b;
+                                if (b == 'Y') {
+                                    cout << "Choose a customization for customer: (1) Lemon (2) No Ice"
+                                    cin >> custom;
+                                    switch (custom) {
+                                        case 1 :
+                                            //add lemon
+                                        case 2 :
+                                            //No ice
+                                    }
+                                }
+                        }
 ///Expand food menu, use switch to add customizing on order, push end result to customer's vector
                     break;
-                case 2 :
-//        drink
-                    cout << "Drink" << endl;
+                case 2 : //Drink
+                    cout << "What drink? (1) Water (2) Soda"
+                    cin >> choiceb;
+                    switch (choiceb) {
+                        case 1 : //Water
+                            cout << "Customize order? (Y/N)" << endl;
+                            cin >> b;
+                            if (b == 'Y') {
+                                cout << "Choose a customization for customer: (1) Lemon (2) No Ice"
+                                cin >> custom;
+                                switch (custom) {
+                                    case 1 :
+                                        //add lemon
+                                    case 2 :
+                                        //No ice
+                                }
+                            }
+                        case 2 : //Soda
+                            cout << "Customize order? (Y/N)" << endl;
+                            cin >> b;
+                            if (b == 'Y') {
+                                cout << "Choose a customization for customer: (1) Extra Large (2) No Ice"
+                                cin >> custom;
+                                switch (custom) {
+                                    case 1 :
+                                        //Make large
+                                    case 2 :
+                                        //No ice
+                                }
+                            }
+                    }
 ///Expand drink menu, use switch to add customizing on order, push end result to customer's vector
+                    cout << "Customize order? (Y/N)" << endl;
+                    cin >> a;
+                    if (a == 'Y') {
+                        cout << "Choose a customization for customer: (1) Lemon (2) No Ice"
+                        cin >> drink;
+                            switch (drink) {
+                                case 1 :
+                                    //add lemon
+                                case 2 :
+                                    //No ice
+                            }
+                    }
                     break;
                 default :
                     break;
@@ -66,6 +218,3 @@ void Orders()
     splitBills();
 }
 
-int customize (){
-
-}
